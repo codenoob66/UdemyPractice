c7096ed151b59444a6bda3a141be3d486706c404
rafael vincent <smurf4812@gmail.com>
2025-05-19 17:07:57 +0000

    hey

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -1,2 +1,9 @@
-import { randomSuperHero} from "superHeroes"
-const name = randomSuperHero()
+// import { randomSuperHero} from "superHeroes"
+// const name = randomSuperHero()
+
+import generateName from "sillyname"
+
+const sillyName = generateName();
+
+
+console.log(sillyName)

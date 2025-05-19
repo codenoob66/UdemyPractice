dba24e9c05172528c2a9cb28c26a151085cf7cb7
rafael vincent <smurf4812@gmail.com>
2025-05-19 16:40:02 +0000

    hey

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,8 +1,12 @@
 const { error } = require("console")
 const fs = require("fs")
 
-
-fs.writeFile("message2.txt","Hello form lineage", (err) => {
-    if(err) throw error;
+fs.writeFile("message.txt", "hello from the otherside", (error) => {
+    if(error) throw error;
     console.log("file has been saved")
-});
+})
+
+fs.readFile("message.txt", "utf-8", (err, data) => {
+    if(err) throw err;
+    console.log(data)
+})

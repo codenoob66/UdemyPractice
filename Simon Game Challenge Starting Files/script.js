c75568a8807b022051eacd6d41f654a36888cdcc
rafael vincent <167981239+codenoob66@users.noreply.github.com>
2025-05-27 16:50:02 +0000

    hey

--- Simon Game Challenge Starting Files/script.js
+++
@@ -1,8 +0,0 @@
-const redBtn = document.getElementById("red")
-const blueBtn = document.getElementById("blue")
-const greenBtn = document.getElementById("green")
-const yellowBtn = document.getElementById("yellow")
-
-function randomBlink() {
-    
-}

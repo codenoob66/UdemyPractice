0391d92856715ec1122839cfaf8e58fd97070de9
rafael vincent <smurf4812@gmail.com>
2025-05-28 19:50:55 +0800

    hey

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -35,6 +35,7 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    this.style.color = "white";
     let audio = new Audio("sounds/tom-1.mp3");
     audio.play();
   });

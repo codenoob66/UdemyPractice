1c14feeb382f3da6bfcab970167aaf3bc1e0cef8
rafael vincent <167981239+codenoob66@users.noreply.github.com>
2025-06-22 11:31:00 +0000

    hey

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,13 @@
-$("h1").css("color", "red")
+$(document).ready(function() {
+    $("body").keydown(function(event) {
+        let currentText = $("h1").text();
+        
+        if (event.key === "Backspace") {
+            $("h1").text(currentText.slice(0, -1));
+        } else {
+            $("h1").text(currentText + event.key);
+        }
+        
+        console.log(event.key);
+    });
+});

814d5084e87154244f9747b2259a0e105eeea2dd
rafael vincent <167981239+codenoob66@users.noreply.github.com>
2025-06-22 13:02:18 +0000

    hey

--- Simon+Game+Challenge+Step+1+Answer/Simon Game Challenge Step 1 Answer/game.js
+++ Simon+Game+Challenge+Step+1+Answer/Simon Game Challenge Step 1 Answer/game.js
@@ -1,1 +1,15 @@
-alert("hello");
+const buttonColor = ["red", "blue", "green", "yellow"]
+const gammePattern = []
+
+
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    const randomChosenColor = buttonColor[randomNumber]
+    $("#" + randomChosenColor).delay(100).fadeOut().fadeIn('slow')
+    gammePattern.push(randomChosenColor)
+    console.log(randomNumber)
+    console.log(randomChosenColor)
+    console.log(gammePattern)
+}
+nextSequence();

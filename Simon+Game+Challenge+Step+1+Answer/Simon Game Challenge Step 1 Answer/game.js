ba01f1b874e4c39e386739e3dff2426302f03f9b
rafael vincent <167981239+codenoob66@users.noreply.github.com>
2025-06-23 00:47:04 +0000

    hey

--- Simon+Game+Challenge+Step+1+Answer/Simon Game Challenge Step 1 Answer/game.js
+++ Simon+Game+Challenge+Step+1+Answer/Simon Game Challenge Step 1 Answer/game.js
@@ -1,15 +1,78 @@
 const buttonColor = ["red", "blue", "green", "yellow"]
-const gammePattern = []
-
-
+const gamePattern = []
+let  userClickedPattern = []
+let gameStarted = false;
+let level = 0;
 
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
     let randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColor = buttonColor[randomNumber]
-    $("#" + randomChosenColor).delay(100).fadeOut().fadeIn('slow')
-    gammePattern.push(randomChosenColor)
+    playSound(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    gamePattern.push(randomChosenColor)
     console.log(randomNumber)
     console.log(randomChosenColor)
-    console.log(gammePattern)
+    console.log(gamePattern) 
 }
+
 nextSequence();
+
+$(".btn").click(function() {
+  let userChosenColour = $(this).attr("id")
+  userClickedPattern.push(userChosenColour)
+  playSound(userChosenColour)
+  console.log(userClickedPattern)
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function playSound(name) {
+    let audio = new Audio("sounds/" + name +  ".mp3")
+    audio.play()
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+
+$(document).keypress(function() {
+  if (!gameStarted) {
+    gameStarted = true;
+    level = 0;
+    gamePattern.length = 0;
+    nextSequence();
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      console.log("success");
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+      // No need to reset userClickedPattern here, it's done in nextSequence()
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern.length = 0;
+  gameStarted = false;
+}

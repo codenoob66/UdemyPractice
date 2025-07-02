a51d8222132ba8677c4d24dbab7d97a2aec48057
rafael vincent <smurf4812@gmail.com>
2025-07-02 17:11:55 +0800

    hey

--- Backend/3.4 Middleware/index1.js
+++ Backend/3.4 Middleware/index1.js
@@ -9,6 +9,10 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.post("/submit", (req, res) => {
   console.log(req.body);
 });

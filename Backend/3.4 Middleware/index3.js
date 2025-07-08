28a37d8aba1c36ef997caffba7bd23b3c53e42a0
rafael vincent <167981239+codenoob66@users.noreply.github.com>
2025-07-08 05:49:20 +0000

    hey

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -1,17 +1,15 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-function logger(req, res) {
+function logger(req, res, next) {
   console.log("Request Method:", req.method)
-  console.log("Response url:", res.url)
+  console.log("Response url:", req.url)
   next()
 }
 
 app.use(logger);
-
 
 app.get("/", (req, res) => {
   res.send("Hello");

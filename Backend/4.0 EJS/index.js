054c55ef8623980d77a955710a39af80ef0fa69a
rafael vincent <smurf4812@gmail.com>
2025-07-15 15:01:16 +0800

    hey

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -4,13 +4,12 @@ const app = express();
 const port = 3100;
 
 app.get("/", (req, res) => {
-  const day = new Date().getDay();
-  console.log(day);
-  if (day > 0 && day < 6) {
-    res.send("hello its a weekday my friends");
-  } else {
-    res.send("hi its a weekend");
-  }
+  const today = new Date().getDay();
+  console.log(today);
+  res.render("index.ejs", {
+    dayType: "a weekday",
+    advice: "it's time to work hard",
+  });
 });
 
 app.listen(port, () => {

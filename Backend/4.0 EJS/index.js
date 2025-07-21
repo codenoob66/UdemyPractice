daebcbb713cc301040999d73e647717397ecc7fe
rafael vincent <d749107@gmail.com>
2025-07-22 04:22:00 +0800

    hey

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -6,10 +6,18 @@ const port = 3100;
 app.get("/", (req, res) => {
   const today = new Date().getDay();
   console.log(today);
-  res.render("index.ejs", {
-    dayType: "a weekday",
-    advice: "it's time to work hard",
-  });
+
+  if (today === 0 || today === 6) {
+    res.render("index.ejs", {
+      dataType: "the weekend",
+      advice: "its time to relax",
+    });
+  } else {
+    res.render("index.ejs", {
+      dataType: "a weekday",
+      advice: "it's time to work hard",
+    });
+  }
 });
 
 app.listen(port, () => {
